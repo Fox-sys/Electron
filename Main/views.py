18614becabccc8b5682e888ca4aeae860db69152
Boris <BorisBerestov@ya.ru>
2020-02-13 18:37:06 +0300

    v-2.0

--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404, render_to_response
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from django.http import HttpResponse
 from .forms import *
